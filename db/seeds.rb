5dbcb240e5cc1453470df3966d85b2a19a91295b
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-03-10 01:31:28 +0000

    develop (#6)
    * innit commit
    
    * init
    
    * Delete dump.rdb
    
    * Feature/graphql (#1)
    
    * innit commit
    
    * init
    
    * Delete dump.rdb
    
    * init with graphql and graphiql
    
    * Feature/graphql (#2)
    
    * innit commit
    
    * init
    
    * Delete dump.rdb
    
    * init with graphql and graphiql
    
    * graphql added with friendlyID
    
    * Feature/cors (#5)
    
    * init commit
    
    * removed manifest graphiql

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
+6.times do
   news_article = NewsArticle.create(title: Faker::Lorem.sentence(word_count:3), body: Faker::Lorem.paragraph(sentence_count:3))
 end
 
-10.times do
+6.times do
   testimonial = Testimonial.create(title: Faker::Lorem.sentence(word_count:3), body: Faker::Lorem.paragraph(sentence_count:3))
 end
