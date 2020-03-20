136620169a251bdcfaa977d831ae1c9f3680fd8c
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-03-20 00:41:42 +0000

    users db and query

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
+  
   root to: 'static#index'
 
   namespace :admin do
     root to: 'main#index'
     resources :news_articles
     resources :testimonials
+    resources :users
   end
 end
